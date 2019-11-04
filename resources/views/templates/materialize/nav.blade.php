75e6906ea23c34edc83a9a891d938c471b31e926
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-11-04 17:46:00 +0330

    Cleaned up materialize/nav

--- resources/views/templates/materialize/nav.blade.php
+++ resources/views/templates/materialize/nav.blade.php
@@ -1,24 +1,18 @@
 <div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper container">
-			<a href="#!" class="brand-logo">{{ $apptitle }}</a>
-			<ul class="right hide-on-med-and-down">
-				<li><a href="sass.html">Sass</a></li>
-				<li><a href="badges.html">اجزا</a></li>
-			</ul>
+			<a href="{{ URL::to('/') }}" class="brand-logo">{{ $apptitle }}</a>
+			<div class="navmenu right hide-on-med-and-down">
+				<ul>
+					<li><a href="#">آیتم ۱</a></li>
+				</ul>
+			</div>
 
-			<ul class="left">
-				<li><a href="">System</a></li>
-				<li>
-					<a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right"></i></a>
-				</li>
-			</ul>
-			<ul id="dropdown1" class="dropdown-content">
-				<li><a href="#!">one</a></li>
-				<li><a href="#!">two</a></li>
-				<li class="divider"></li>
-				<li><a href="#!">three</a></li>
-			</ul>
+			<div class="topmenu left">
+				<ul>
+                    <li><a href="/login">Login</a></li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 </div>
