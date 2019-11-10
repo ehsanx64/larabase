494a7f6cc9e8672bfc1728e69cc16ac70a8a23e4
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-11-10 12:22:49 +0330

    Added comments for base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,23 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * This class is the base class for both front-end and back-end controllers and defines methods
+ * useful for both sides.
+ *
+ * Class Controller
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController {
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+	/**
+	 * This method is used for making things a little easier. It will detect the requested controller
+	 * and loads the given view from a directory with the same name as the controller.
+	 *
+	 * @param $viewfile string Name of view to be rendered
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
 	protected function render($viewfile) {
 		$target = strtolower(str_replace('Controller', '',
 			class_basename(app('request')->route()->controller)));
