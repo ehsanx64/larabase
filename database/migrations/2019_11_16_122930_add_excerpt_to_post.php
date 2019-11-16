69551752a35236d44ef896d247b4e41f0a0fa0f7
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-11-16 16:11:54 +0330

    Added some migrations for Post entity; required doctrine/dbal too

---
+++ database/migrations/2019_11_16_122930_add_excerpt_to_post.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExcerptToPost extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('post', function (Blueprint $table) {
+			$table->text('excerpt')->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('post', function (Blueprint $table) {
+			$table->removeColumn('excerpt');
+		});
+	}
+}
