68021283d825516c168825c4f90038c88abcf97f
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-11-16 16:32:04 +0330

    Refactored post to posts

--- database/migrations/2019_11_16_122705_create_posts_table.php
+++ database/migrations/2019_11_16_122705_create_posts_table.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostTable extends Migration {
+class CreatePostsTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
-		Schema::create('post', function (Blueprint $table) {
+		Schema::create('posts', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('title');
 			$table->text('content');
@@ -26,6 +26,6 @@ class CreatePostTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists('post');
+		Schema::dropIfExists('posts');
 	}
 }
--- database/migrations/2019_11_16_122930_add_excerpt_to_posts.php
+++ database/migrations/2019_11_16_122930_add_excerpt_to_posts.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddExcerptToPost extends Migration {
+class AddExcerptToPosts extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
-		Schema::table('post', function (Blueprint $table) {
+		Schema::table('posts', function (Blueprint $table) {
 			$table->text('excerpt')->nullable();
         });
 	}
@@ -22,7 +22,7 @@ class AddExcerptToPost extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::table('post', function (Blueprint $table) {
+		Schema::table('posts', function (Blueprint $table) {
 			$table->removeColumn('excerpt');
 		});
 	}
