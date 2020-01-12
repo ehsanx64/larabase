47ae367ca85df04ce2e38104414ff9b31a622028
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-12 23:53:55 +0330

    Breadcrumb and page header updated

--- resources/views/templates/admin-lte/page.blade.php
+++ resources/views/templates/admin-lte/page.blade.php
@@ -59,25 +59,35 @@
 	<div class="content-wrapper">
 		<!-- Content Header (Page header) -->
 		<section class="content-header">
-			<h1>
-				{{ $title }}
-				<small>{{{ $subTitle or '' }}}</small>
-			</h1>
-			<ol class="breadcrumb">
-				<li>
-					<a href="{{ url('/admin') }}"><i class="fa fa-dashboard"></i>پنل مدیریت</a>
-				</li>
-				<li class="active">
-					@if (isset($mainLink))
-						<a href="{{ $mainLink }}">{{ $title }}</a>
-					@else
-						{{ $title }}
-					@endif
-				</li>
-				@if (isset($subTitle))
-					<li>{{ $subTitle }}</li>
-				@endif
-			</ol>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>
+							{{ $title }}
+							<small>{{{ $subTitle or '' }}}</small>
+						</h2>
+						<ol class="breadcrumb">
+							<li>
+								<a href="{{ url('/admin') }}">
+									<i class="fa fa-dashboard"></i>&nbsp;{{ __('Control Panel') }}</a>
+							</li>
+							<li class="active">
+								@if (isset($mainLink))
+									<a href="{{ $mainLink }}">{{ $title }}</a>
+								@else
+									{{ $title }}
+								@endif
+							</li>
+							@if (isset($subTitle))
+								<li>{{ $subTitle }}</li>
+							@endif
+						</ol>
+
+					</div>
+
+				</div>
+			</div>
+
 		</section>
 
 		<!-- Main content -->
