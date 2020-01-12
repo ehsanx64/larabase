4e4f65cc69b62946dd961f9a15918a62749982e6
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-12 19:39:24 +0330

    Refactored templates/admin-lte/_header

--- resources/views/templates/admin-lte/page.blade.php
+++ resources/views/templates/admin-lte/page.blade.php
@@ -46,56 +46,7 @@
 <div class="wrapper">
 
 	<header class="main-header">
-		<!-- Logo -->
-		<a href="{{ url('/') }}" class="logo">
-			<!-- mini logo for sidebar mini 50x50 pixels -->
-			<span class="logo-mini">پنل</span>
-			<!-- logo for regular state and mobile devices -->
-			<span class="logo-lg"><b>کنترل پنل مدیریت</b></span>
-		</a>
-		<!-- Header Navbar: style can be found in header.less -->
-		<nav class="navbar navbar-static-top">
-			<!-- Sidebar toggle button-->
-			<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-				<span class="sr-only">Toggle navigation</span>
-			</a>
-
-			<div class="navbar-custom-menu">
-				<ul class="nav navbar-nav">
-					<!-- User Account: style can be found in dropdown.less -->
-					<li class="dropdown user user-menu">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<img src="{{ $assets }}/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-							<span class="hidden-xs">{{ Auth::user()->name }}</span>
-						</a>
-						<ul class="dropdown-menu">
-							<!-- User image -->
-							<li class="user-header">
-								<img src="{{ $assets }}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-								<p>
-									{{ Auth::user()->name }}
-									<small>مدیریت کل سایت</small>
-								</p>
-							</li>
-							<!-- Menu Footer-->
-							<li class="user-footer">
-								<div class="pull-left">
-									<a href={{ route('logout') }}" onClick=" event.preventDefault();document.getElementById(
-									'logout-form').submit();" class="btn btn-default
-									btn-flat">خروج</a>
-
-									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-										{{ csrf_field() }}
-									</form>
-								</div>
-							</li>
-						</ul>
-					</li>
-					<!-- Control Sidebar Toggle Button -->
-				</ul>
-			</div>
-		</nav>
+		@include("$template/_header")
 	</header>
 
 	<!-- right side column. contains the logo and sidebar -->
