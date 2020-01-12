7b0c275d25305b33f4eed30db1a9e92f53673d77
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-12 18:41:14 +0330

    Updated main nav items

--- resources/views/templates/materialize-parallax/_nav.blade.php
+++ resources/views/templates/materialize-parallax/_nav.blade.php
@@ -3,13 +3,13 @@
 		<a id="logo-container" href="{{ $app_url }}" class="brand-logo">{{ $app_title }}</a>
 		<ul class="right hide-on-med-and-down">
 			<li>
-				<a href="#">Navbar Link</a>
+				<a href="/loose">Loose</a>
 			</li>
 		</ul>
 
 		<ul id="nav-mobile" class="sidenav">
 			<li>
-				<a href="#">Navbar Link</a>
+				<a href="/loose">Loose</a>
 			</li>
 		</ul>
 		<a href="#" data-target="nav-mobile" class="sidenav-trigger">
