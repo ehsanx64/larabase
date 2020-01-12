e736ca3368a7d7945b69a130d307deeccffa486d
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-12 23:32:19 +0330

    Updated dashboard url

--- resources/views/templates/admin-lte/page.blade.php
+++ resources/views/templates/admin-lte/page.blade.php
@@ -65,7 +65,7 @@
 			</h1>
 			<ol class="breadcrumb">
 				<li>
-					<a href="{{ url('/') }}"><i class="fa fa-dashboard"></i>پنل مدیریت</a>
+					<a href="{{ url('/admin') }}"><i class="fa fa-dashboard"></i>پنل مدیریت</a>
 				</li>
 				<li class="active">
 					@if (isset($mainLink))
